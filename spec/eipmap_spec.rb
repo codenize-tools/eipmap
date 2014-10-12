0190b873d6e1bfb0265798a0d7cc3d9c9e81264d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-13 02:38:29 +0900

    Fix spec

--- spec/eipmap_spec.rb
+++ spec/eipmap_spec.rb
@@ -1,5 +1,7 @@
 describe Eipmap do
   it do
     expect(1).to eq 1
+    pp describe_instances
+    pp describe_addresses
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,13 +20,14 @@ def run_instances(n)
 
   instance_ids = resp.instances.map(&:instance_id)
   $ec2.wait_until(:instance_running, instance_ids: instance_ids)
-  $test_instance_ids = instance_ids
+  $test_instances = resp.instances
 end
 
 def terminate_instances
-  return unless $test_instance_ids
-  $ec2.terminate_instances(instance_ids: $test_instance_ids)
-  $ec2.wait_until(:instance_terminated, instance_ids: $test_instance_ids)
+  return unless $test_instances
+  instance_ids = $test_instances.map(&:instance_id)
+  $ec2.terminate_instances(instance_ids: instance_ids)
+  $ec2.wait_until(:instance_terminated, instance_ids: instance_ids)
 end
 
 def allocate_addresses(n)
@@ -54,6 +55,47 @@ def disassociate_addresses
     next unless association_id
     $ec2.disassociate_address(association_id: association_id)
   end
+end
+
+def describe_instances
+  return {} unless $test_instances
+  result = {}
+
+  $test_instances.each do |instance|
+    instance_id = instance.instance_id
+    result[instance_id] = {}
+    interface = instance.network_interfaces.first
+    result[instance_id][:network_interface_id] = interface.network_interface_id
+    result[instance_id][:private_ip_addresses] = {}
+
+    interface.private_ip_addresses.each do |address|
+      ip = address.private_ip_address
+      primary = address.primary
+      result[instance_id][:private_ip_addresses][ip] = primary
+    end
+  end
+
+  result
+end
+
+def describe_addresses
+  result = {}
+
+  $ec2.describe_addresses.each do |resp|
+    resp.addresses.each do |address|
+      domain = address.domain
+      public_ip = address.public_ip
+      result[domain] ||= {}
+      result[domain][public_ip] = {}
+
+      [:instance_id, :network_interface_id, :private_ip_address].each do |key|
+        value = address[key] || ''
+        result[domain][public_ip][key] = value unless value.empty?
+      end
+    end
+  end
+
+  result
 end
 
 RSpec.configure do |config|
