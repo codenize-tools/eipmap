218b383817f49f346716b8a61261a24722f4dbee
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-13 13:52:49 +0900

    Add spec

--- spec/eipmap_spec.rb
+++ spec/eipmap_spec.rb
@@ -116,4 +116,22 @@ end
       expect(describe_addresses["vpc"]).to eq swapped_eips_with_one_assoc
     end
   end
+
+  context "when dry-run" do
+    subject { client(dry_run: true) }
+
+    it do
+      dsl = <<-EOS
+domain "vpc" do
+<%- eips.each do |public_ip, attrs| -%>
+  ip "<%= public_ip %>", <%= attrs.inspect %>
+<%- end -%>
+end
+      EOS
+
+      result = apply(subject) { dsl }
+      expect(result).to be_falsey
+      expect(describe_addresses["vpc"].values).to match_array [{}, {}, {}]
+    end
+  end
 end
