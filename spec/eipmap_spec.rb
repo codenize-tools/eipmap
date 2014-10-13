366aba32eb3b2744cac65a87942556617ae8288d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2014-10-13 13:00:11 +0900

    Fix spec

--- spec/eipmap_spec.rb
+++ spec/eipmap_spec.rb
@@ -1,9 +1,17 @@
 describe Eipmap do
+  let(:eips) do
+    eips = describe_addresses["vpc"].keys.zip(
+             describe_network_interface.map {|interface_id, private_ips|
+               { network_interface_id: interface_id,
+                 private_ip_address: private_ips.first}})
+    Hash[*eips.flatten]
+  end
+
   context "when no association" do
     it do
       dsl = <<-EOS
 domain "vpc" do
-<%- describe_addresses["vpc"].keys.each do |public_ip| -%>
+<%- eips.each do |public_ip, attrs| -%>
   ip "<%= public_ip %>"
 <%- end -%>
 end
@@ -16,14 +24,6 @@ end
   end
 
   context "when associate" do
-    let(:eips) do
-      eips = describe_addresses["vpc"].keys.zip(
-               describe_network_interface.map {|interface_id, private_ips|
-                 { network_interface_id: interface_id,
-                   private_ip_address: private_ips.first}})
-      Hash[*eips.flatten]
-    end
-
     it do
       dsl = <<-EOS
 domain "vpc" do
@@ -36,6 +36,34 @@ end
       result = apply { dsl }
       expect(result).to be_truthy
       expect(describe_addresses["vpc"]).to eq eips
+    end
+  end
+
+  context "when disassociate" do
+    before do
+      apply {
+        <<-EOS
+domain "vpc" do
+<%- eips.each do |public_ip, attrs| -%>
+  ip "<%= public_ip %>", <%= attrs.inspect %>
+<%- end -%>
+end
+        EOS
+      }
+    end
+
+    it do
+      dsl = <<-EOS
+domain "vpc" do
+<%- eips.each do |public_ip, attrs| -%>
+  ip "<%= public_ip %>"
+<%- end -%>
+end
+      EOS
+
+      result = apply { dsl }
+      expect(result).to be_truthy
+      expect(describe_addresses["vpc"].values).to match_array [{}, {}, {}]
     end
   end
 end
